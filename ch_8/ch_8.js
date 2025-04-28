b4d9c38cb17a94219ca273cf533b44e924774132
FreedomEagle217 <jacobochristopher743@gmail.com>
2025-04-28 16:47:07 -0600

    Added Ch 8 and an intro

---
+++ ch_8/ch_8.js
@@ -0,0 +1,10 @@
+//  Monday April 28, 2025  4:42 p.m.   Christopher Jacobo
+
+/*
+                    JavaScript BUILT-IN METHODS
+
+    Much like several java functions are widely available without importing
+        (eg, System.out.print, Math.pow(), etc ), JavaScript also has widely
+        available functions and methods. These "global" were determined long 
+        ago by the language designers
+*/
